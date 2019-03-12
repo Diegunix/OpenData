2c7f76d1e791c4e9f685bfc1396fb57cdb60497a
Diego Palomo <diegopalomo@gmail.com>
2019-03-12 15:47:29 +0100

    Add endpoints

--- src/main/java/com/opendata/controller/AirController.java
+++ src/main/java/com/opendata/controller/AirController.java
@@ -3,37 +3,47 @@ package com.opendata.controller;
 import java.io.IOException;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.opendata.dao.domain.Air;
+import com.opendata.dao.domain.Estacion;
 import com.opendata.dao.repository.AirRepository;
 import com.opendata.service.OpenDataService;
 
 @RestController
 public class AirController {
-    
-private AirRepository airRepository;
-private OpenDataService service;
 
-    
+    private AirRepository airRepository;
+    private OpenDataService service;
+
     public AirController(AirRepository airRepository, OpenDataService service) {
         this.airRepository = airRepository;
         this.service = service;
     }
 
-  
-  @GetMapping(value = "/data")
-  public @ResponseBody Iterable<Air> getAllWeather() {
-      return this.airRepository.findAll();
-  }
-  
-  
-  @GetMapping(value = "/importData")
-  public @ResponseBody Iterable<Air> getData() throws IOException {
-      this.service.getData();
-      return null;
-  }
-  
-  
+    @GetMapping(value = "/air")
+    public @ResponseBody Iterable<Air> getAllAir() {
+        return this.airRepository.findAll();
+    }
+
+    @GetMapping(value = "/air/ano/{ano}/mes/{mes}/dia/{dia}")
+    public @ResponseBody Iterable<Air> getAllAirByDate(@PathVariable("ano") int ano, @PathVariable("mes") int mes,
+            @PathVariable("dia") int dia) {
+        return this.airRepository.findAllByAnoAndMesAndDia(ano, mes, dia);
+    }
+
+    @GetMapping(value = "/air/estacion/{estacion}/ano/{ano}/mes/{mes}/dia/{dia}")
+    public @ResponseBody Iterable<Air> getAllAirByEstacionAndDate(@PathVariable("estacion") Long estacion, @PathVariable("ano") int ano,
+            @PathVariable("mes") int mes, @PathVariable("dia") int dia) {
+        return this.airRepository.findAllByEstacionAndAnoAndMesAndDia(Estacion.builder().id(estacion).build(), ano, mes, dia);
+    }
+
+    @GetMapping(value = "/importData")
+    public @ResponseBody Iterable<Air> getData() throws IOException {
+        this.service.getData();
+        return null;
+    }
+
 }
--- src/main/java/com/opendata/dao/repository/AirRepository.java
+++ src/main/java/com/opendata/dao/repository/AirRepository.java
@@ -1,8 +1,16 @@
 package com.opendata.dao.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.opendata.dao.domain.Air;
+import com.opendata.dao.domain.Estacion;
 
 public interface AirRepository extends JpaRepository<Air, Long> {
+    
+    List<Air> findAllByEstacionAndAnoAndMesAndDia(Estacion estacion, int ano, int mes , int dia);
+    
+    List<Air> findAllByAnoAndMesAndDia(int ano, int mes , int dia);
+    
 }
